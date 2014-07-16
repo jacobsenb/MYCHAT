1211a1afe77b83401519c6d398c93fc59676a2d8
Brett Jacobsen <jacobsenb@gmail.com>
2014-07-16 14:11:14 +1200

    Client

---
+++ MyChat.Client/Controllers/Session.js
@@ -0,0 +1,8 @@
+﻿app.controller('SessionController', function ($resource) {
+
+    this.Test = "123";
+    
+    this.testing = function myFunction() {
+        alert("I am an alert box!");
+    }
+});
---
+++ MyChat.Client/Services/MyChatService.js
@@ -0,0 +1,11 @@
+﻿app.factory('MyChatService', function ($resource) {
+    var requestUri = 'http://win-qci5apqetrc/MyChat/';
+
+    return $resource(requestUri, {},
+        {
+            "register": { method: 'POST', url: "http://win-qci5apqetrc/AngularService/api/Account/Register" },
+            "userInfo": { method: "GET", url: "http://win-qci5apqetrc/AngularService/api/Account/UserInfo" },
+            "signIn": { method: "POST", params: { isPersistent: 'false' }, url: "http://win-qci5apqetrc/AngularService/api/Account/SignIn" }
+        });
+
+});
